061816e9a56a2e871aa7f2d5ffbdcc3a76b9673e
JensDiemer <git@jensdiemer.de>
2012-08-10 21:14:02 +0200

    add djangobb_code_comments urls if installed

--- djangobb_plugin/urls.py
+++ djangobb_plugin/urls.py
@@ -7,17 +7,22 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-#sitemaps = {
-#    'forum': SitemapForum,
-#    'topic': SitemapTopic,
-#}
+
+try:
+    import djangobb_code_comments
+except ImportError:
+    djangobb_code_comments = False
+else:
+    djangobb_code_comments = True
+
 
 urlpatterns = patterns('',
-    # Sitemap
-#    (r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
-
-    # Apps
     (r'^account/', include('django_authopenid.urls')),
     (r'^pm/', include('django_messages.urls')),
     (r'^', include('djangobb_forum.urls', namespace='djangobb')),
 )
+
+if djangobb_code_comments:
+    urlpatterns += patterns('',
+        (r'^', include('djangobb_code_comments.urls')),
+    )
